293158d29595040a3965456b97c62e37f32923dd
Opilane TTHK <opilane@tthk.ee>
2020-10-08 11:27:34 +0300

    hahahaha

---
+++ Params/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamsSum(50, 50);
+            ParamsWord("Apple");
+
+        }
+
+        public static void ParamsSum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+
+        public static void ParamsWord(string c)
+        {
+            Console.WriteLine(c);
+        }
+
+        //public static void ParamsArray(int[] = {"pear", "grapes", "oranges", "banana", })
+        //{
+        //
+        //}
+            
+    }
+}
---
+++ Week6Functions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Transactions;
+
+namespace Week6Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloUser();
+            SumOfTwo();
+        }
+
+        public static void HelloUser()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}");
+        }
+
+        public static void SumOfTwo()
+        {
+            Console.WriteLine("Enter the first number: ");
+            double Number = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            double Number2 = Int32.Parse(Console.ReadLine());
+            double Answer = Number + Number2;
+            Console.WriteLine($"The sum of the numbers is {Answer}");
+        }
+    }
+}
